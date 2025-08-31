caf7a95452c40ea78909505b429bf4a56d05c145
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-08-30 19:42:35 -0700

    Homework_is Chill

---
+++ Homework.py
@@ -0,0 +1,9 @@
+import math
+
+tuple1 = (4,3,2,2,-1,18)
+t1p = math.prod(tuple1)
+print("The product of",tuple1,"is",t1p)
+
+tuple2 = (2,4,8,8,3,2,9)
+t2p = math.prod(tuple2)
+print("The product of",tuple2,"is",t2p)
